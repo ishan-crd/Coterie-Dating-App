fe855e135053db649076462914bb39dc5dcbed11
ishan-crd <ishangupta3121@gmail.com>
2026-06-28 13:10:57 +0530

    Initial Commit

---
+++ coterie-ios/coterie_iosApp.swift
@@ -0,0 +1,32 @@
+//
+//  coterie_iosApp.swift
+//  coterie-ios
+//
+//  Created by Ishan Gupta on 28/06/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct coterie_iosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
